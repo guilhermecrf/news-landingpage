91ecc6026385ccbad21f5b770f3e108de7d0218c
Guilherme Santana <guilhermedrxs@gmail.com>
2024-04-24 20:30:58 -0300

    project finished ✨✅

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+// var
+
 const openButton = document.querySelector('header > button')
 const closeButton = document.querySelector('header nav button')
 
 const body = document.querySelector('body')
 const nav = document.querySelector('header .mobile')
 
+// events
+
 openButton.addEventListener('click', openMenu)
 closeButton.addEventListener('click', closeMenu)
+
+// functions
 
 function openMenu() {
     body.classList.add('dark')
